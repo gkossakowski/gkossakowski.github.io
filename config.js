27b3b4d3b12fd8b0cfaa4f4a70252c1c8237d283
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2020-04-11 00:11:56 +0200

    fix url in gkk.dev

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://gkk.dev',
   pathPrefix: '/',
   title: 'Blog by Grzegorz Kossakowski',
   subtitle: 'Subtitle: TBD',
